27ab771cb0f2a0b1f383745288fa1c4946f0237d
AbdoulRaouf2005 <abdoulraoufharouna62@gmail.com>
2025-05-21 19:10:31 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-read dossier
-if [ -d "../$dossier" ]; then
-      compteur=$(ls -1|wc -l)
-      echo "Le dossier $dossier contient $compteur fichier(s)."
-      exit 0
-else
-      exit 1
-fi
