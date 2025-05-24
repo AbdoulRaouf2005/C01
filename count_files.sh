2b7fcc1c23dd77b289889b357dc6b6ec92d7fe95
AbdoulRaouf2005 <abdoulraoufharouna62@gmail.com>
2025-05-24 02:21:45 +0100

    Add files via upload

--- count_files.sh
+++ count_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read dossier
 if [ -d "../$dossier" ];then
-      fichier=$(ls -1 ../$dossier)
-      echo "$fichier" > temp.txt
-      nb=$(wc -l < temp.txt)
-      rm temp.txt
+      nb=$(ls ../$dossier | wc -l)
       echo "Le dossier $dossier contient $nb fichier(s)."
+else 
+      echo "Le dossier '$dossier' n'existe pas."
+      exit 1
 fi
 exit 0
--- find_string.sh
+++ find_string.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 read -p "Entrez le nom du fichier : " fichier
 
-read -p "Entrez le mot à chercher dans $fichier : " Recherche
-
 if [ -f "$fichier" ]; then
+      read -p "Entrez le mot à chercher dans $fichier : " Recherche
       if grep -q "$Recherche" ${fichier}; then
             echo "La chaîne '$Recherche' a été trouvée dans ${fichier}."
       else
            echo "La chaîne '$Recherche' n'a pas été trouvée dans ${fichier}." 
       fi
+else
+      echo "Le fichier '$fichier' n'existe pas."
+      exit 1
 fi
 exit 0    
