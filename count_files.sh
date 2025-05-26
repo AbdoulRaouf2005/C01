ae86bf117c63ebd53c866322c37cd3deea67cc4a
AbdoulRaouf2005 <abdoulraoufharouna62@gmail.com>
2025-05-26 07:16:36 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 read dossier
-if [ -d "../$dossier" ];then
-      nb=$(ls ../$dossier | wc -l)
-      echo "Le dossier $dossier contient $nb fichier(s)."
-else 
-      echo "Le dossier '$dossier' n'existe pas."
-      exit 1
-fi
+nb=$(ls "$dossier" | wc -l)
+echo "Le dossier $dossier contient $nb fichier(s).
 exit 0
